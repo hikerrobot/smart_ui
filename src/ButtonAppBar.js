37d011e7ee694706b53f6e2c1750f5bfa69a767c
hikerrobot <hikerrobot@gmail.com>
2021-02-01 20:28:51 +0000

    Updated app nav bar text.

--- src/ButtonAppBar.js
+++ src/ButtonAppBar.js
@@ -35,7 +35,7 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Foo
+            Smart Monkey
           </Typography>
         </Toolbar>
       </AppBar>
